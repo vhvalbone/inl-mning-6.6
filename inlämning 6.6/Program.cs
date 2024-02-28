b46d255a1d85d6874a2cb7d62e1bd7c1880751f6
vhvalbone <albin06svensson@gmail.com>
2024-02-28 10:27:47 +0100

    Add project files.

---
+++ inlämning 6.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SkrivUtVartannatOrd("Detta är ett exempel. Texten ändras när den skrivs ut.");
+    }
+
+    static void SkrivUtVartannatOrd(string input)
+    {
+        string[] ord = input.Split(' ');
+
+        for (int i = 0; i < ord.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(ord[i].ToLower() + " ");
+            }
+          
+            else
+            {
+                Console.Write(ord[i].ToUpper() + " ");
+            }
+        }
+
+        Console.WriteLine(); 
+    }
+}
